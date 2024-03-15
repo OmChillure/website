3b5e687ab818f04e3f46a3753b3f18531749672d
OmChillure <omchillure4@gmail.com>
2024-03-15 16:04:49 +0530

    day2

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -3,10 +3,12 @@ import Carousel from "../Components/Carousel";
 import Cards from "../Components/Cards";
 import Domains from '../Components/Domains'
 import BottomCarousel from '../Components/BottomCarousel';
+import Home from "../Components/Home";
 
 function Homepage() {
   return (
     <>
+      <Home />
       <Carousel />
       <Cards />
       <Domains />
